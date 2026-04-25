ffce0ce2b5484a1f95ab2af5223061c1d1930d91
DCrust369 <DCrust@proton.me>
2026-04-25 23:35:34 +0100

    Create boot_face.rs

---
+++ kernel/boot_face.rs
@@ -0,0 +1,31 @@
+fn main () {
+  let asm: u8 = energy; // ele recebe a energia mas por tanto ainda com 8 bytes de carga
+  let asm: u16 = logo; // ele carrega a logo da marca do computador
+  let asm: u32 = senha; // ele carrega a senha
+  let asm: u64 = interface;
+  {
+    // bloco 1 // block 1
+    let face: u128 = gnu;
+    let face: u64 = gnome; // i use arch NO i use fedora BTW (by to way)
+    let face: u256 = kde_plasma;
+    let face: u512 = rice; // this kernel is a easy kernel for linux user
+    // suporting for interfaces
+    let mut face = gnu;
+    let mut face = gnome;
+    let mut face = kde_plasma;
+    let mut face = xfce;
+    let rice: type = interface_for_your_likes; // sorry i don't speak english
+    // bloco 2 // block 2
+    mod casa_decimal: u1024 = UP;
+    // exemple 16 --> 32 --> 64
+    use::files;
+    use::wake_upC.asm;
+    use::wake_up_C2.asm;
+    // ajustes
+    let mut security_boot = false; // i am not corporation
+    let BIOS: type = true;
+    let NoPanic.zig = true;
+    use::NoPanic.zig;
+    // no segmentation fault or kernel panic but it's my errors
+  }
+}
