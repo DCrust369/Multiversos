715ce6b030f43d22987c0dbd16cb11cca022fbd1
DCrust369 <DCrust@proton.me>
2026-04-25 22:30:03 +0100

    Create resistDDOS.zig

---
+++ resistDDOS.zig
@@ -0,0 +1,21 @@
+const std = @import ("std");
+const fn main () void {
+    const very_informations: type = DDOS; // ok the compiler stop it every informations 
+    const e-mail: type = very_e-mails;
+    const fn DOSinstructions.forCPU.GPU ({
+        struct informations.email: type = (blocker);
+        struct login: u8 = (blocker);
+        struct enterON.site.mesage: u8 (blocker);
+        const OneInformations.per.10SECONDS: type = true;
+        const server OPERATIONS.informatios: u32 = CPU(.{});
+        const OPERATIONS.asm: u32 = SSD(.{});
+    });
+    const fn DDOSinstructions.forCPU.GPU ({
+        struct logs.enters: u64 = MAXI.USE.CPU(.{});
+        struct mesages.enter.etc: u64 = MIXI.USE_CPU_GPU_SSD_HS(.{});
+        const otimizate.instructions: type = true;
+        const use_asm_C_binary: u64 = 0110111101110100011010010110110101101001011110100110010100100000010000110101000001010101001000000110000101101110011001000010000001000111010100000101010100100000011000010110111001100100001000000100100001000100(.{});
+        const MOV_EAX_ADD_c,p,u: u64 = EAX_=_379(.{});                                                                                                                                                                                                                                                                                                    // se dividir este numero (que é o 379) da o resultado de 126,333333333 e se fazer o caminho inverso da 378,999999999 // o hacker vai ficar confuso                                                                                                                                                                                                                                                                                                                                                          // por exemplo eu fiz 379 div 3 = 126,333333333 então 126,333333333 x 3 = 378,999999999 que é 0,1 a menos boa sorte meu hackerzinho  // e quem faz essa conta quando eu fiz MOV EAX ADD c,p,u o CPU é basicamente dividir por 3                                                                                                                                                                                                                                                                   
+    });
+    // my math it's a blocker of de DDOS
+}
