ba6f06bbfa4d85b2a838c053d356d67f3436dd10
DCrust369 <DCrust@proton.me>
2026-04-26 10:19:15 +0100

    Update driver_hardware.zig

--- drives_PC/driver_hardware.zig
+++ drives_PC/driver_hardware.zig
@@ -1,1 +1,19 @@
-const 
+const std = @import ("std");
+const fn main () void {
+    const nvidea: u64 = detect;
+    const intel: u64 = detect;
+    const amd: u64 = detect
+    const other: 128 = detect;
+    const fn drivesHARDWARE ({
+        const asm: u64 = mapHARDWARE;
+        const telemetry = asm;
+        const asm = clear;
+        const clear = telemetry;
+        const shor_C = true;
+        const asm = true
+        const hardware: short_C = CPU;
+        const hardware: shoort_C = GPU;
+        const hardware: short_C = Network_Card;
+        const hardware: short_C = (asm(motherboard));
+    });
+}
