0bf62788330fa85e1f8308b91307df3a1905917f
DCrust369 <DCrust@proton.me>
2026-05-02 15:03:01 +0100

    Create leyer_notes.zig

---
+++ leyernet_programming_language/leyer_notes.zig
@@ -0,0 +1,21 @@
+const std = @import("std"); // i whiting a leyernet language
+
+pub fn main ()void {
+	.const asm: type = asm_controler;
+	.const Clang: type = C_controler;
+	.const RS: type = rust_controler;
+	.const zig: type = zig_controler;
+};
+
+pub fn keyboards(allocate: u64) void {
+	.var mov: bool = (move_bite_for_hash);
+	.var mut: bool = (manipule_value);
+	.var &mut: bool = (no_mutable);
+  .const term: bool (value_shell);
+  // documentations of the language leyernet
+  // this is a new assembly
+  .const printik: type = see_value;
+  .const chain: type = start;
+  .const yum: bool = (start);
+  .const gnu: bool = start;
+};
