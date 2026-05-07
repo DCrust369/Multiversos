bbad059dc2297c9ca7f983633453a020ab77c82e
DCrust369 <DCrust@proton.me>
2026-05-07 20:01:55 +0100

    Create How_to_use.lua

---
+++ tools/Fênix/How_to_use.lua
@@ -0,0 +1,12 @@
+# Phrase
+
+print("scaner end reverse enginer tool")
+
+# How to use
+
+local name_site = "sudo lixer -key www.exemple.com"
+local analyzer = "search lixer -key exemple.rs"
+
+# Phrase
+
+print("The quieter you become, the more invasive and destructive you become")
