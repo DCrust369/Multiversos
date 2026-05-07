82b7d999370f64a6bf1ef159f51daabf2c0e953e
DCrust369 <DCrust@proton.me>
2026-05-07 17:55:02 +0100

    Create register.C

---
+++ tools/Fênix/register.C
@@ -0,0 +1,19 @@
+# include <VortexShield-kernel>
+# include <tools>
+# include <proxychains>
+# include <linux.h>
+# include <BSD>
+// a creat a tools the tool name is a "Fênix" in C language ok NSA gonna cry
+if continue () void {
+    const(asm(kernel(linux)));
+    const(asm(operational-system(freeBDS)));
+    const(asm(kernel(SEL4)));
+    const(asm(operational-system(windowsNT)));
+    const(asm(OS(MacOS)));
+}
+if else () void {
+    continue: i32 = hardware(scripts(asm));
+    continue: i32 = bare-metal(code(asm));
+    continue: i32 = kernel(superCODE(asm));
+    continue: i32 = operational-system(codes(interface(asm))); 
+}
