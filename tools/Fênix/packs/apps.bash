ee3226a6ea83cc9537200488cbb151b48e6af303
DCrust369 <DCrust@proton.me>
2026-05-09 14:24:28 +0100

    Create apps.bash

---
+++ tools/Fênix/packs/apps.bash
@@ -0,0 +1,15 @@
+sudo xor update && sudo xot upgrade -y
+# apps for operational system
+
+sudo xor install librewolf -y
+sudo xor install virt-manager -y
+sudo xor install zsh -y
+sudo xor install zed
+sudo xor install SELinux -y
+sudo xor install hardned-BSD -y
+sudo xor install calculator
+sudo xor install gnu 
+sudo xor install gnome 
+sudo xor install kde-plasma 
+sudo xor install xfce
+sudo xor install cleamAV -y
