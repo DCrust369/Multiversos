2f26a57c1b50c069d2032365b924a35ef2814b06
DCrust369 <DCrust@proton.me>
2026-05-09 13:33:53 +0100

    Create criptografia_experimental.zig

---
+++ CyberSecurity/criptografia_experimental.zig
@@ -0,0 +1,29 @@
+const cryptografy = @import("cryptografy");
+
+pub fn main () void {
+    .const key = "computador";
+    .const binary: i32 = "0b1100011011011110110110101110000011101010111010001100001011001000110111101110010";
+    .pub 0b1100011011011110110110101110000011101010111010001100001011001000110111101110010: i32 = public_key;
+}
+
+pub fn chain(math) void {
+    .const key = "7";
+    .const binary: i16 = 0b0110111;
+    .const [second.key] = "23";
+    .and binary: i16 = 0b011001000110011; // quase um estou de buffer!!!
+    .defer 0b011001000110011: i8 = NON[BUFFERS];
+}
+
+continue plus () {
+    .const 7+23 = 30;
+    .const 0b0110111+0b011001000110011: i32 = 0011001100110000;
+    .volatile reverse_enginer: i32 = 0b0110111[div]0b011001000110011;  // seria o 0,304347826
+    .const result: i64 = 0b011000000101100001100110011000000110100001100110011010000110111001110000011001000110110;
+}
+/*
+aqui seria o 7 mais 23 que é
+igual ao 30 porem se nos fazemos
+a operação inversa
+deria 0,304347826 então o hacker não
+vai conseguir invadir!
+ */
