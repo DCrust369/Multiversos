2ef6fbe02043700441c4dc322620b02219ad3917
DCrust369 <DCrust@proton.me>
2026-05-10 15:22:41 +0100

    Update apps.bash

--- tools/Fênix/packs/apps.bash
+++ tools/Fênix/packs/apps.bash
@@ -7,17 +7,23 @@ sudo xor install waterfox -y
 # but waterfox exist!
 sudo xor install virt-manager -y
 sudo xor install zsh -y
-sudo xor install zed
+# for x84
+sudo xor install zed -y
 # add acode for arm
 sudo xor install acode -y
+
 sudo xor install SELinux -y
 sudo xor install hardned-BSD -y
 # no hardned bsd no selinux in arm
 sudo xor install lineageOS -y
 sudo xor install grapheneOS -y
-sudo xor install calculator
+
+sudo xor install calculator -y
+
 sudo xor install gnu 
 sudo xor install gnome 
 sudo xor install kde-plasma 
 sudo xor install xfce
+
+# anvi malware
 sudo xor install cleamAV -y
