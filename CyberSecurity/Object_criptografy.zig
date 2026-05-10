dd9dada82f238c39d985cde4386c4dce6dbec766
DCrust369 <DCrust@proton.me>
2026-05-10 16:34:48 +0100

    Create Object_criptografy.zig

---
+++ CyberSecurity/Object_criptografy.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+
+pub fn main () void {
+    .const objects: type = object-1;
+    .const objects: type = object-2;
+    .const objects: type = object-3;
+    .const objects: type = object-4;
+    .const objects: type = object-5;
+}
+
+pub fn Color*mul-Shuffle () {
+    .const mul*object-1 = object-5;
+    /*
+    the object 1
+    mul for object 5 es it's
+    1*5 or 2*5 or 3*5 this is
+    logic is a simple math
+    */
+    .const mul*objects-2: type = object-10;
+    .const mul*objects-3: type = object-15;
+    .const mul*objects-4: type = object-20;
+    .const mul*objects-5: type = object-25;
+    .volatile objects: i64 = (object-10(object-15(object-20)));
+    .volatile object-25: type = key(pass);
+}
+
+pub continue colour () ({
+    .const colour: i32 = (10(15(20(25))));
+    .const [NON-DIFERENCES]: type = true;
+    /*no have diferences
+    in keys but your key
+    is a key for system
+    */
+    if 25: i32 = user(computer(workstation));
+});
