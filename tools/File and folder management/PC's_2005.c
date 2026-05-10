c699545a3fc0e742b3665b8cce09fe0e292e1711
DCrust369 <DCrust@proton.me>
2026-05-10 21:03:44 +0100

    Create PC's_2005.c

---
+++ tools/File and folder management/PC's_2005.c
@@ -0,0 +1,12 @@
+#include <512-MB>
+#include <60-GB> // Para o HD
+/*
+agora é mais facil
+*/
+continue asm () void {
+    const 512mb: i32 = (apps(files)); // faltou 3 para ter um buffer overflow usei i32 mas deu 29 letras
+    const 60gm: i64 = (operational-system);
+}
+/*
+para PC's de 2005
+*/
