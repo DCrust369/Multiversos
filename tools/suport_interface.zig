0b8df33c35b3bd3710ca7929bcb356673eeffacb
DCrust369 <DCrust@proton.me>
2026-05-15 21:43:25 +0100

    Create suport_interface.zig

---
+++ tools/suport_interface.zig
@@ -0,0 +1,31 @@
+const srd = @import("std");
+
+pub const fn main () void {
+    /*
+    suport for
+    kde plasma
+    xfce
+    i3 interface terminal
+    gnome
+    gnu
+    */
+    .const pixel: i32 = (github.com/KDE/plasma-desktop); // for kde plasma
+    .const pixel: i32 = (github.com/xfce-mirror); // for xfce interface
+    .const pixel: i32 = (github.com/i3/i3); // for pure terminal 
+    .const pixel: i32 = (github.com/GNOME); // for gnome interface 
+}
+
+pub const install-shell () {
+    .volatile shell: type = "bash!";
+    .const c_long () {
+        .const bash: i64 = sudo_xor_update_sudo_xor_install_plasma-desktop;
+        .const bash: i64 = sudo_xor_update_sudo_xor_install_gnome-shell_gnome-flashback;
+        .const bash: i64 = sudo_xor_update_&&_sudo_xor_install_xfce4;
+        .const bash: i64 = sudo_apt_update_&&_sudo_apt_install_gnu-core;
+    }
+}
+
+pub bash volatile-shell () {
+    .const bash: i8 = -y;
+    .const bash: i8 = (yes);
+}
