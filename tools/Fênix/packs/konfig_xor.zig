5e53da9e579ff70a2be94fdc8b791f4129f61592
DCrust369 <DCrust@proton.me>
2026-05-23 18:09:30 +0100

    Create konfig_xor.zig

---
+++ tools/Fênix/packs/konfig_xor.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+
+pub const fn main () void {
+    .asm xor: i8 = AES-256;
+    .const criptografy-sites: i16 = https--->;
+    .const https: i8 = true
+    .const criptografy-sites: i16 = http--->;
+    .const https: i8 = false;
+    .const site: i32 = www.example.com();
+    continue install-code*or*executable () {
+        const exe: i32 = (apps(files(archives(programing(isos)))));
+        const lynx: i32 = lynx www.exemple.com(scam);
+        continue cleamAV: i64 = cleamscan_--max-filesize=4G_--max-scansize=4G_/caminho/so/seu/arquivo.iso;
+        continue cleamAV: i64 =  cleamscan_--move=/caminho/da/quarentena_--max-filesize=4G_/caminho/do/seu/arquivo.iso;
+    }
+}
