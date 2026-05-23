831d97b94753b65bffbaf32371061fdeab72053a
DCrust369 <DCrust@proton.me>
2026-05-23 11:35:10 +0100

    Create ClearCash.zig

---
+++ leyernet_programming_language/memory.leyernet_tech/ClearCash.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+
+pub const fn main() void {
+    // i creat the clearcash
+    // i so tool for leyernet programing language
+    // for 100 or 50 lines have 1 clearcash
+    // it's a manual memory and garbedge colector
+    .const code: i32 = acumulator(cash);
+    .asm lines: i32 = 100(lines(MAX));
+    .asm OR-lines: i32 = 50(lines(mini));
+}
+
+continue c_long () {
+    .const clearcash: i16 = restart(memory); // 16 words for i16
+    .const code: i32 = 16gb(reset(cash=));
+    .const 16gb: i8 = 8gb;
+    // espace memory
+    // it's a security memory
+    // of the rust but a frendom
+    // of the C (or zig)
+}
+
+continue c_long active-safe-or-non-active-safe() {
+    .const active-safe: i8 = (rs(=));
+    .const rs: i32 = compiler(rustc);
+    .const c_shor {
+        .const non-active-safe: i8 = (c(=));
+        .const c: = compiler(gcc); // gnu project
+    }
+}
