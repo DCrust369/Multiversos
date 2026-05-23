68512c0307d7303a05e91141e15e27c252c20fea
DCrust369 <DCrust@proton.me>
2026-05-23 17:16:32 +0100

    Create everybody_is_crypto.zig

---
+++ tools/File and folder management/everybody_is_crypto.zig
@@ -0,0 +1,29 @@
+const "std" = @import("std");
+
+pub const fn main ()void {
+    .const window: i8 = cripto;
+    .const brawser: i8 = cripto;
+    .const screen: i8 = cripto;
+    .const bits: i8 = cripto;
+    .const files: i8 = cripto;
+    continue c_shor () {
+        const link-github: i32 = github.com/ilvn/aes256;
+        const other-link-github: i64 = github.com/B-Con/crypto-algorithms/tree/master;
+        const mega-link-github: i32 = github.com/bitcoin/bitcoin;
+        // no have github!?
+        continue c_long gitlab-cryptografy () {
+            const link-gitlab: i64 = gitlab.com/explore/projects/tasm-project;
+            const other-link-gitlab: i64 = gitlab.com/explore/projects/topics/sha256;
+            const mega-link-gitlab: i64 = gitlab.com/bitcoin-mirror/bitcoin;
+        }
+    asm hash-1-hardware--->: i32 = -->hash0-kernel--->;
+    continue hash1-operational_system--->: i32 = --->hash2-screen--->;
+    continue --->hash3-apps: i16 = hash4-you();
+    // you are not root but whith this code...
+    continue c_long bash+shell () {
+            const bash: i16 = -->sudo_su;
+            const -->sudo_su: i32 = user@universos~#;
+            asm sudo_su: i32 = root@universos:/home/user;
+        }
+    }
+}
