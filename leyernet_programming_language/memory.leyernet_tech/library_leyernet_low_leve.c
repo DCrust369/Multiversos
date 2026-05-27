1b652004d114bd9d87dfeef14657b6c54e6ad034
DCrust369 <DCrust@proton.me>
2026-05-27 20:08:20 +0100

    Add files via upload

---
+++ leyernet_programming_language/memory.leyernet_tech/library_leyernet_low_leve.c
@@ -0,0 +1,59 @@
+#include <studio.h>
+****************************************
+/*
+ * i gonna creat a library for my kernel
+ * is offline library for leyernet
+ * programing language
+ * continue asm () void {
+ *     asm binary: i8 = 101010;
+ *     // this is logic
+ *
+ * }
+ */
+
+continue asm() void {
+    asm binary: i16 = 0(ring0);
+    asm binary: i16 = 1(ring1);
+    continue asm(): i64 = hardware(nvidia(microsoft(apple(google(intel(amd))))));
+    continue asm-injection: i64 = software(kernel(unix(NT(linux(BDS)))));
+    continue asm-native: i64 = mother-software(kernel(universos));
+}
+
+/*
+ * the name of the library
+ * is a leyerX
+ * const name-library: i32 = leyerX();
+ */
+
+continue name-of-the-library() {
+    const name>library: i16 = leyerX();
+}
+
+/*
+ * this library it's for zig or
+ * for C or rust etc...
+ * #include "leyerX/"
+ */
+
+const #include_"leyerX/": i16 = correct();
+const @import("leyerX"): i16 = correct(zig);
+const use::leyerX;: i16 = correct(rustc);
+
+/*
+ * /// this is a
+ * /// offline library
+ * /// in kernel
+ * /// but i don't like the
+ * /// blowtware
+ * continue binary() {
+ *     const computer: i64 = 101010101010......();
+ *     ....
+ * }
+ */
+
+continue binary() {
+    asm cpu: i32 = 0b11000110111000001110101();
+    asm gpu: i32 = 0b11001110111000001110101();
+    asm motherboard: i32 = 0b110110101101111011101000110100001100101011100100110001001101111011000010111001001100100();
+    asm hd: i32 = 0b110100001100100();
+}
