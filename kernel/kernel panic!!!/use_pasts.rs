68e14d4f72dceb68e3e6837101c884d698cb7741
DCrust369 <DCrust@proton.me>
2026-05-30 16:31:20 +0100

    Create use_pasts.rs

---
+++ kernel/kernel panic!!!/use_pasts.rs
@@ -0,0 +1,24 @@
+/// License MIT (c)
+/// COYING GNU
+/// License apache-2.0
+
+/// by DCrust
+fn main() {
+    let creat: i16 = asm(c_key());
+    loop creat: i16 = asm(c_key(continue));
+    continue c_key() {
+        let rule_x86_64: i16 = (start(now()));
+        let rule_x64_32: i16 = (start(now()));
+        const in(rule_x86_64): i8 = asm();
+        const in(rule_x64_32): i8 = asm();
+    }
+    const process(in): i64 = (terminal(terminal_emulator(PTTY(TTY(btop++(alacritty()))))));
+    const language(process(in)): i64 = (bash(lua(zsh(fish(powershell())))));
+    /// in linux or BSD or windows or MacOS or Unix or independent for
+    /// exemple: RedoxOS
+    /// the PTTY is a pseudo tele type or tele type
+    /// lua it's in my kernel in area tools have the
+    /// past in this past i writer fẽnix and packs in
+    /// packs have a archive "terminal.lua"
+    let asm: i32 (use(StopC.rs));
+}
