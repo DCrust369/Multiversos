12a1cb48cea2cd9fade4e5029ed733f8f729e7c8
DCrust369 <DCrust@proton.me>
2026-05-30 21:57:33 +0100

    Create keylangs.c

---
+++ leyernet_programming_language/memory.leyernet_tech/keywords/keylangs.c
@@ -0,0 +1,23 @@
+#include <studio.h>
+
+continue asm keywords() void {
+    asm assm: i32 = (assembly(instructions));
+    asm Ckey: i32 = (C(instructions));
+    asm zigzag: i32 = (zig(instructions));
+    asm rust: i32 = (rust(instructions));
+    asm go!: i32 = (golang(instructions));
+    asm 01: i64 = (start(binary(of(the(zero(and(ones())))))));
+    /*
+    it's a keylangs
+    philosofy
+    my programing language leyernet
+    the compiler is a
+    Golang
+    Rust
+    Assembly
+    C
+    but not binary
+    i am human not robot
+    */
+    
+}
