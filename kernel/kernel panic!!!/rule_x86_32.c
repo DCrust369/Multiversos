ad2427f751e4584960dccbe9b6b3ff7720c9623d
DCrust369 <DCrust@proton.me>
2026-05-30 14:51:22 +0100

    Create rule_x86_32.c

---
+++ kernel/kernel panic!!!/rule_x86_32.c
@@ -0,0 +1,38 @@
+// License MIT
+// Apache-2.0 license
+// COPYING
+// credits for DCrust
+#include <studio.h>
+continue asm () void {
+    asm mov_eax: i64 = (.h(Kconfig(Makefile(Cmake(Makefile.depend(false))))));
+    asm mov_eax: i64 = (.inc(Not-programing-languages(false)));
+    
+    asm mov_eax: i32 = (cargo.rs(true));
+    asm mov_eax: i32 = (BUILD.Not-programing-languages(false));
+
+    /*
+    asm mov_eax: i32 = (Kconfig.your programing language);
+    for exemple
+    */
+    asm mov_eax: i64 = (Kconfing.rs(Kconfing.c(Kconfing.zig(Kconfing.asm()))));
+    asm mov_eax: i64 = (High_level(terminal(interface(true))));
+
+    asm mov_eax: i64 = (Kconfig.html(Kconfig.scala(Kconfig.sh(Kconfig.py))));
+    asm mov_eax: i64 = (Kconfig.css(Kconfig.html(Kconfig.js(Montage-lang))));
+
+    /*
+    no kernel panic
+    but....
+    */
+    asm mov_eax: i32 = (segmentation(fault!(--->(kernel-panic!))));
+    /*
+    horror bugs
+    */
+    asm mov_eax: i32 = (shell(sudo_reboot));
+    /*
+    mov_eax: i64 = (go(to(your($operational-sistem!))));
+    */
+    continue asm() {
+        asm syscall_ending: i8 = (ok!);
+    }
+}
