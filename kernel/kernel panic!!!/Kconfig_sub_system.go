272c82c7f560c351206037ec8bfbb287b21b8383
DCrust369 <DCrust@proton.me>
2026-05-30 16:45:26 +0100

    Create Kconfig_sub_system.go

---
+++ kernel/kernel panic!!!/Kconfig_sub_system.go
@@ -0,0 +1,15 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main () {
+	var config = (modules(hardnedBSD(SELinux(grapheneOS(LinageOS())))))
+	var config = mod(sub-system)
+	var Kconfig = git_push;
+	var git_push = git_push_github.com/LineageOS/android_hardware_oplus.git
+	var git_push = git_push_github.com/GrapheneOS/hardened_malloc.git
+	var git_push = git_push_github.com/SELinuxProject/selinux.git
+	var git_push = git_push_github.com/HardenedBSD/hardenedBSD.git
+}
