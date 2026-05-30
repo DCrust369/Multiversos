eafed589211f84be79a8d2146536dedf88ba07da
DCrust369 <DCrust@proton.me>
2026-05-30 16:25:01 +0100

    Create commands.lua

---
+++ tools/Fênix/packs/commands.lua
@@ -0,0 +1,38 @@
+print("$your-sistem@$USER")
+
+if else {
+    local creat = paste
+}
+if else {
+    local creat = paste
+}
+if else {
+    local sudo = password-admin
+}
+if else {
+    local xor = package
+}
+if else {
+    local install = push(executable)
+}
+if else {
+    local upgrade = atualization
+}
+if else {
+    local update = mini(atualizations)
+}
+if else {
+    local sinx = notes
+}
+if else {
+    local git_push = push_codes
+}
+if else {
+    local cd = enter(file)
+}
+if else {
+    local ls = your_files
+}
+break now = end
+# ...
+# continue
