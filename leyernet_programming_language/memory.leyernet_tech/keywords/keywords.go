a9b71bc568407fc3df4bee538cf1e9e616e1fe22
DCrust369 <DCrust@proton.me>
2026-05-30 21:47:26 +0100

    Create keywords.go

---
+++ leyernet_programming_language/memory.leyernet_tech/keywords/keywords.go
@@ -0,0 +1,35 @@
+// MIT license (c)
+// APACHE-2.0
+// COYING of the gnu
+// by DCrust
+package main
+
+import "fmt"
+
+func main() {
+	/// i creat the rules in golang
+	/// programing language
+	/// in my language it's so
+	/// very, very KISS man
+	/// it's so good
+	const ; int = stop
+	const : int = memory
+	const >> int = plus
+	const << int = less
+	var + int = math_operation_plus
+	var - int = math_operation_less
+	var * int = math_operation_times
+	var % int = math_operation_Percentage
+	var $ int = math_operation_money
+	const mover int = bit1>>bit2>>bit3>>...
+	const pausa int = stop
+    const map {
+		/// for keywords
+		const {} = start_or_end
+		const smooth = start
+		var publ int = public_value
+		var ciber int = (wake(up(value)))
+		const criminal int = (independent(value))
+		var boxes int = (virtual-box(for(tests)))
+	}
+}
