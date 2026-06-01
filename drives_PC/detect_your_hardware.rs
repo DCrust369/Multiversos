8f3b0fdbc0ab2b2ee6558320251738cfb8e3648e
DCrust369 <DCrust@proton.me>
2026-06-01 20:23:01 +0100

    Create detect_your_hardware.rs

---
+++ drives_PC/detect_your_hardware.rs
@@ -0,0 +1,88 @@
+/// License MIT
+/// Apache
+/// GPU general public license
+/// DCrust [01/06/2026]
+fn main() {
+    /// simulator of the assembly
+    /// programing language
+    let mov_rax: i64 = (pendrive(HD-external(virtual-machine())));
+    move 32gb: i32 = (32gb(pendrive()));
+    let mut 32gb: i32 = (partition(16gb(8gb)));
+    /*
+    * mov rax ; for 64 registors
+    * mov eax ; for 32 registors
+    * mov ax ; for 8 registers
+    */
+    let mov_eax: i32 = (pendrive(HD-external(virtual-machine())));
+    move 32gb: i32 = (32gb(pendrive()));
+    let mut 32gb: i32 = (partition(16gb(8gb)));
+    /*
+    * and now i writer
+    * the 
+    * mov ax for 8 bits register
+    * in rust but is a "assembly simulator"
+    */
+    let mov_ax: i32 = (pendrive(HD-external(virtual-machine())));
+    move 32gb: i32 = (32gb(pendrive()));
+    let mut 32gb: i32 = (partition(16gb(8gb)));
+    /*
+    * the diferences of the
+    * gb, gb is a memory
+    * espace the 
+    * 8bit or 16bits or 32bits or 64bits
+    * it's a register
+    */
+    let mut mov_rax: i32 = (partition(8gb(16gb(32gb(64gb)))));
+    let mut mov_rax: i32 = host(pendrive(ram-memory));
+    let mut mov_rax: i32 = program(ventoy(banela-etcher(rufus())));
+    /*
+    * for old PC's whith
+    * 32 bits
+    */
+    let mut mov_eax: i32 = (partition(8gb(16gb(32gb(64gb)))));
+    let mut mov_eax: i32 = host(pendrive(ram-memory));
+    let mut mov_eax: i32 = program(ventoy(banela-etcher(rufus())));
+    /*
+    * whith a 
+    * PC's whith 8bits
+    */
+    let mut mov_ax: i32 = (partition(8gb(16gb(32gb(64gb)))));
+    let mut mov_ax: i32 = host(pendrive(ram-memory));
+    let mut mov_ax: i32 = program(ventoy(banela-etcher(rufus())));
+    /*
+    * detect a hardware
+    * this is a...
+    * nvidia?
+    * intel?
+    * amd?
+    * microsoft?
+    * google?
+    */
+    let mut mov_rax: i16 = scam(...);
+    let mut mov_rax: i32 = installDRIVES(nvidia(intel(microsoft(google(amd)))));
+    let mut detect: i32 = instalibites?(y(n(?)));
+    let mut y: i32 = (shell(zsh(bash(powershell))));
+    /*
+    * for 32 bits PC
+    */
+    let mut mov_eax: i16 = scam(...);
+    let mut mov_eax: i32 = installDRIVES(nvidia(intel(microsoft(google(amd)))));
+    let mut detect: i32 = instalibites?(y(n(?)));
+    let mut y: i32 = (shell(zsh(bash(powershell))));
+    /*
+    * for 8bits systens
+    */
+    let mut mov_ax: i16 = scam(...);
+    let mut mov_ax: i32 = installDRIVES(nvidia(intel(microsoft(google(amd)))));
+    let mut detect: i32 = instalibites?(y(n(?)));
+    let mut y: i32 = (shell(zsh(bash(powershell))));
+}
+
+/// sudo -s
+/// password:
+/// sudo xor remove <driver>
+/// sudo xor install <drive>
+/// sudo xor update && sudo xor upgrade -y
+/// mkdir drives?
+/// creat a file whith name drives
+/// it's so good for organization
