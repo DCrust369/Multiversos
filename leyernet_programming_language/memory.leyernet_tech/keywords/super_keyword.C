69344ad1ee44faa478f6e542fe132e0870160c82
DCrust369 <DCrust@proton.me>
2026-06-01 21:18:04 +0100

    Create super_keyword.C

---
+++ leyernet_programming_language/memory.leyernet_tech/keywords/super_keyword.C
@@ -0,0 +1,40 @@
+/*
+License MIT
+License GNU
+Apache 2.0
+DCrust [01/06/2026]
+*/
+continue asm () {
+    const fn: i32 = start(your(code));
+    const main: i32 = other(starter);
+    /*
+    i use the KISS philosofy
+    */
+    const beat: i32 = manipule(memory)
+    const it: i32 = (true-or-false(manipule(memory)));
+    const they: i32 = move(bits(from(x)));
+    /*
+    others 
+    keywords
+    */
+    const smooth_criminal: i32 = (kill(data));
+    const michel: i32 = (memory(CPU));
+    const jackson: i32 = (memory(GPU));
+    /*
+    especial keywords
+    */
+    const win: i32 = (automatize(memory(whith(rust))));
+    const lin: i32 = (manual(memory(whith(zig))));
+    /*
+    continue asm () {
+    }
+    */
+    asm mov_rax: i32 = (ker(=(dont-stop))); // never experiment a (ker(=(don't(stop)))); ok
+    asm mov_rax: i32 = (eternel(=(brute-force)));
+
+    asm mov_rax: i32 = (uni(=(unity-values)));
+    asm mov_rax: i32 = (net(=(connect-values)));
+    /*
+    final syscall
+    */
+}
