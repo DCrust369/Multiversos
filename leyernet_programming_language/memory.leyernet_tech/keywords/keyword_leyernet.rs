c82fe22acbd70844639ac2700ad0c31ea7c7a81f
DCrust369 <DCrust@proton.me>
2026-06-03 12:06:59 +0100

    Add files via upload

---
+++ leyernet_programming_language/memory.leyernet_tech/keywords/keyword_leyernet.rs
@@ -0,0 +1,30 @@
+// Copyright MIT (c) 2026 DCrust. All Rights Reserved.
+// Copyright Apache (c) 2026 DCrust. All Rights Reserved
+// Copyright GNU (c) 2026 DCrust. All Rights Reserved.
+fn main() {
+    let :: i16 = (before(memory));
+    let ;: i32 = (end(value));
+    let "let": i16 = (normal(value));
+    let "imut": i32 = (imutable(value));
+    let "mov": i32 = (mov(bits));
+    // i creat a philosofy
+    // pixel-by-bit PBB
+    const struct () {
+        let "pixel"-"bit"-"byte": i32 = (cubes(memory(or(screen(binary)))));
+        // what it's a pixel
+        let "pixel": i32 = (cube(screen())); // for example gnome, KDE plasma or XFCE
+        let "byte": i32 = (cube(memory())); // the i32 it's a memory
+        let "bit": i32 = (cube(binary())); // the binary it's a zero's and one's
+        // keywords
+        let "pix": i32 = (interface(mov));
+        let "by": i32 = (manual(security(memory()))); // MSM
+        let "bit": i32 = (before(0101010(have(bit()))));
+        /// manual memory but i use rust
+        /// interface whith a compiled language not montage language
+        /// zero's and one's whith language not low level
+        let "pix": i16 = (keyword);
+        let "by": i16 = (keyword);
+        let "bit": i16 = (keyword);
+    }
+    // end
+}
