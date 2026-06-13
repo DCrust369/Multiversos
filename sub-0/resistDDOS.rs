c8190f1b3d3ca1532e28ef083b13c34e241854c7
DCrust369 <DCrust@proton.me>
2026-06-13 22:00:00 +0000

    Add files via upload

---
+++ sub-0/resistDDOS.rs
@@ -0,0 +1,18 @@
+// Copyright MIT (c) 2026 DCrust. All Rights Reserved.
+// Copyright APACHE (c) 2026 DCrust. All Rights Reserved.
+// Copyright GNU (c) 2026 DCrust. All Rights Reserved.
+mod informations
+mod logs
+
+fn main () {
+    let hacker(>>): i32 = (host2());
+    if else () {
+        let informations(+10000): i32 = blocker(force);
+    }
+    const buffer_overflow(): i16 = 1234567890123(); // 16 for i16 plus one it's a buffer overflows but rust don't like overflows
+    const PC_home(): i8 = true;
+    if else () {
+        let hacker(>>): i32 = (host3());
+        let informations(+120): i32 = blocker(force);
+    }
+}
