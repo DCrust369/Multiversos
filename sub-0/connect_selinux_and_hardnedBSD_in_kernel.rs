59d39457488b4dd39f9a86f8683614b0fedd1e0c
DCrust369 <DCrust@proton.me>
2026-06-13 21:44:54 +0000

    Add files via upload

---
+++ sub-0/connect_selinux_and_hardnedBSD_in_kernel.rs
@@ -0,0 +1,20 @@
+mod hardned_BSD
+mod SELinux
+mod connect
+
+fn main () {
+    let conneBSDSE(): i32 = $SHELL(zsh(bash(lua())));
+    let config/connect/conneBSDSE(): i32 = execute(files());
+    if else () {
+        const bash(): i32 = wget_-O_repo.zip_github.com/HardenedBSD/hardenedBSD();
+        const bash(): i32 = wget_-O_repo.zig_github.com/SELinuxProject/selinux();
+    }
+    const connect(): i32 = file(bash(=));
+    if else () {
+        let connect: i32 = archive(directory_executable.sh());
+        let directory_executable.sh(): i8 = true;
+    }
+    let security(): i32 = true;
+    let security2(criptografy()): i8 = true;
+    let security3(resist()): i8 = true;
+}
