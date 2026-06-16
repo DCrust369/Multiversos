a92aa799e73c9533b41f64194d6420c32b199f63
DCrust369 <DCrust@proton.me>
2026-06-16 23:00:53 +0100

    Update encript.rs

--- xor_package/encript.rs
+++ xor_package/encript.rs
@@ -1,1 +1,19 @@
+mod push_internet
 
+mod cryptografy
+
+mod e2eee
+
+fn main() {
+    let internet(>>): i32 = https(true);
+    let internet(>>): i32 = http(false);
+    if else () {
+        const key(96543470613): i32 = qwedgklipcm(encript());
+        const internet(>>): i32 = algorit(simbol(true));
+        const simbol: i32 = @#$%*_%!<>=(plus(cript()));
+    }
+    let mut server(>>): i32 = computer(your(host()));
+    if else () {
+        let key(96543470613): i32 = true(package(installed()));
+    }
+}
