5129857e89dcfc5288215f21243b89f1d96f527d
DCrust369 <DCrust@proton.me>
2026-06-19 20:14:18 +0100

    Update NONscript-kiddie.zig

--- CyberSecurity/NONscript-kiddie.zig
+++ CyberSecurity/NONscript-kiddie.zig
@@ -5,28 +5,17 @@
 const NoAtacks = @import("NoAtacks"); () void
 
 pub fn main () {
-    /*criando um codigo que é
-    anti metasploit-framework
-    anti nmap e tals e no final
-    vou dar dicas */
+    // criando um codigo que é
+    // anti metasploit-framework
+    // anti nmap e tals e no final
+    // vou dar dicas
     .const simulation: type = script_kidie;
     .const bash: i32 = msfconsole;
+    .const block(msfconsole): i8 = true;
     .const hacker@kali: bash = search_VortexShield-kernel;
-    .const kernel: zig = blocker; // ele não precissa de muito esforço tenho consiencia que meu kernel é um pouco seguro
-}
-
-pub fn mapeamento() void {
+    .const kernel: i16 = blocker; // ele não precissa de muito esforço tenho consiencia que meu kernel é um pouco seguro
+     //
     .const hacker@kali: bash = sudo_nmap_-sS_VortexShield-kernel;
     .const kiil = [internet];
     print("you are hacked, {s}!\n", .{"kiil switch"});
 }
-
-/*
-as minhas dicas são
-- use uma vpn confiavel
-- tenha um bom senso
-digital
-- use um antivirus
-- atualiza o seu sistema
- */
-// essas dicas são para iniciantes
