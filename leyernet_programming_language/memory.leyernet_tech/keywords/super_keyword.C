49b02da7ccd00e16d7bb75e6e9e02519f09f9078
DCrust369 <DCrust@proton.me>
2026-06-19 19:56:21 +0100

    Update super_keyword.C

--- leyernet_programming_language/memory.leyernet_tech/keywords/super_keyword.C
+++ leyernet_programming_language/memory.leyernet_tech/keywords/super_keyword.C
@@ -4,37 +4,49 @@ License GNU
 Apache 2.0
 DCrust [01/06/2026]
 */
-continue asm () {
-    const fn: i32 = start(your(code));
-    const main: i32 = other(starter);
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+const int main (void) 
+{
+    int32_t fn = start(your(code));
+    const int32_t f* = malloc(sizeof);
+    int32_t main = other(starter);
+    const int32_t m* = malloc(sizeof);
     /*
     i use the KISS philosofy
     */
-    const beat: i32 = manipule(memory)
-    const it: i32 = (true-or-false(manipule(memory)));
-    const they: i32 = move(bits(from(x)));
-    /*
-    others 
-    keywords
-    */
-    const smooth_criminal: i32 = (kill(data));
-    const michel: i32 = (memory(CPU));
-    const jackson: i32 = (memory(GPU));
+    int32_t beat = manipule(memory);
+    const int32_t b* = malloc(sizeof);
+    int32_t it: i32 = (true-or-false(manipule(memory)));
+    const int32_t i* = malloc(sizeof);
+    int32_t they: i32 = move(bits(from(x)));
+    const int32_t t* = malloc(sizeof);
     /*
     especial keywords
     */
-    const win: i32 = (automatize(memory(whith(rust))));
-    const lin: i32 = (manual(memory(whith(zig))));
+    int32_t win: i32 = (automatize(memory(whith(rust))));
+    const int32_t w* = malloc(sizeof);
+    int32_t lin: i32 = (manual(memory(whith(zig))));
+    const int32_t l* = malloc(sizeof);
     /*
     continue asm () {
+    ...
     }
     */
-    asm mov_rax: i32 = (ker(=(dont-stop))); // never experiment a (ker(=(don't(stop)))); ok
-    asm mov_rax: i32 = (eternel(=(brute-force)));
-
-    asm mov_rax: i32 = (uni(=(unity-values)));
-    asm mov_rax: i32 = (net(=(connect-values)));
+    int asm () {
+        int32_t mov_rax: i32 = (ker(=(dont-stop))); // never experiment a (ker(=(don't(stop)))); ok
+        const int32_t m* = malloc(sizeof);
+        int32_t mov_rax: i32 = (eternel(=(brute-force)));
+        const int32_t m* = malloc(sizeof);
+        //
+        int32_t mov_rax: i32 = (uni(=(unity-values)));
+        const int32_t m* = malloc(sizeof);
+        int32_t mov_rax: i32 = (net(=(connect-values)));
+        const int32_t m* = malloc(sizeof);
     /*
     final syscall
     */
+    }
 }
