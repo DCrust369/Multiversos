343fb00e697ffa82adebc4f1b7ba70a7edd88cc7
DCrust369 <DCrust@proton.me>
2026-07-19 18:02:36 +0100

    Update automation.bash

--- tools/automation.bash
+++ tools/automation.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 # direitos autorais DCrust 16/04/2026
+# copyright (C) DCrust 16/04/2026
 
-echo "Selecione o modo de execução:"
+echo "select the execution mode:"
 
-select opcao in "emprestimo_zig" "build_zig" "cargo_run" "zig_run" "sair"; do
-    case $opcao in
+select option in "emprestimo_zig" "build_zig" "cargo_run" "zig_run" "sair"; do
+    case $option in
         "emprestimo_zig") zig run src/main.zig  ;;
         "build_zig")      zig build              ;;
         "cargo_run")      cargo run              ;;
