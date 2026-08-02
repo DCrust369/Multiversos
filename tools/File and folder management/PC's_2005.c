79da97ba13ed91ed94e156cdc81a6104c2298ccb
DCrust369 <DCrust@proton.me>
2026-08-02 17:09:08 +0000

    Update PC's_2005.c

--- tools/File and folder management/PC's_2005.c
+++ tools/File and folder management/PC's_2005.c
@@ -1,10 +1,14 @@
-continue asm () void 
+#include <stdio.h>
+#include "rpmalloc"
 {
-    int32_t 512mb = (apps(files));
-    const int32_t m* = malloc(sizeof);
+    int *e = 512mb;
+    int16_t 512mb = (apps(files));
+    const int16_t m* = rpmalloc(5 + sizeof(int16_t));
     //
+    int *k = 60gm;
     int32_t 60gm = operational_system;
-    const int32_t g* = malloc(sizeof);
+    const int32_t g* = rpmalloc(5 + sizeof(int32_t));
     m(free);
     g(free);
+    return 0;
 }
