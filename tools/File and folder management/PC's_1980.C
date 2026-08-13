39e6ac0fb3215a02aacae6e74ba25914a4279a4c
DCrust369 <DCrust@proton.me>
2026-08-13 19:09:24 +0000

    Update PC's_1980.C

--- tools/File and folder management/PC's_1980.C
+++ tools/File and folder management/PC's_1980.C
@@ -1,13 +1,15 @@
 #include <stdio.h>
-#include "rpmalloc"
+#include <malloc>
 
 const int main(void) {
-    int16_t 16mb = (shell(bash));
-    const int16_t b* = rpmalloc(sizeof(16mb));
+    int16_t 16mb = shell_bash;
+    const int16_t b* = rpmalloc(sizeof(int16_t));
     //
-    int16_t 50mg = (system);
-    const int16_t 50mg s* = rpmalloc(sizeof(50mg));
+    int16_t 50mg = system;
+    const int16_t 50mg s* = rpmalloc(sizeof(int16_t));
     //
-    int16_t 256kb = (interface(i3));
-    const int16_t i* = rpmalloc(sizeof(256kb));
+    int16_t 256kb = interface_i3
+    const int16_t i* = rpmalloc(sizeof(int16_t));
+    
+    return 0;
 }
