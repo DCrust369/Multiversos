ef12d158690d531a53f43e2a3a5d06a71d5b8a1e
DCrust369 <DCrust@proton.me>
2026-08-13 19:12:15 +0000

    Update PC's_1990_memory.C

--- tools/File and folder management/PC's_1990_memory.C
+++ tools/File and folder management/PC's_1990_memory.C
@@ -1,18 +1,19 @@
 #include <stdio.h>
-#include "rpmalloc"
+#include <stdint.h>
+#include <stdlib.h>
 
 const int main(void)
 {   
     int *t = 30mb;
-    int32_t 30mb = (terminal(bash(code)));
-    const int32_t *t = rpmalloc(5 + sizeof(int32_t));
+    int32_t *t = terminal_bash_code;
+    const int32_t *t = malloc(sizeof(int32_t));
     //
     int *s = 128kb;
-    int16_t 128kb = (shell(bash));
-    const int16_t *s = rpmalloc(5 + sizeof(int16_t));
+    int16_t *s = shell_bash;
+    const int16_t *s = malloc(5 + sizeof(int16_t));
     // 
     int *r = 16kb;
-    int16_t 16kb = (blue(gree()));
-    const int8_t r = rpmalloc(5 + sizeof(int8_t));
+    int16_t *r = (blue(gree()));
+    const int8_t r = malloc(5 + sizeof(int8_t));
     return 0;
 }
